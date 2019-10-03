a043c67e642f11b1f6a482896b8c7e52ba1e065b
Martin <boehmmartin2000@yahoo.at>
2019-10-03 08:14:26 +0200

    get and getByName

---
+++ Hue_01/Hue_01/Contact.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace Hue_01
+{
+    public class Contact
+    {
+        public int id { get; set; }
+
+        public String firstname { get; set; }
+
+        public String lastName { get; set; }
+
+        public string email { get; set; }
+    }
+}
---
+++ Hue_01/Hue_01/Controllers/ContactController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Hue_01.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ContactController : ControllerBase
+    {
+        private static readonly List<Contact> contacts=new List<Contact>();
+
+        private readonly ILogger<ContactController> _logger;
+
+        public ContactController(ILogger<ContactController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        [Route("/Contacts")]
+        public IActionResult Get()
+        {
+            return Ok(contacts);
+        }
+        [HttpGet]
+        [Route("/Contacts/findByName")]
+        public IActionResult GetByName([FromQuery][Required()] string nameFilter)
+        {
+            List<Contact> toSend = new List<Contact>();
+            for(var i=0;i<contacts.Count();i++)
+            {
+                if (contacts[i].firstname.Contains(nameFilter)|| contacts[i].lastName.Contains(nameFilter))
+                {
+                    toSend.Add(contacts[i]);
+                }
+            }
+            return Ok(toSend);
+        }
+    }
+}
